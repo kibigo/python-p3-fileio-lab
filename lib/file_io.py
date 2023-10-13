79cccadd98ac01e854c2bb17c1b6b99b45751a6a
kibigo <jacksonkibigo99@gmail.com>
2023-10-13 23:45:55 +0300

    Completed Assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    my_file = open(f"{file_name}.txt", mode='w')
+    my_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    my_file = open(f"{file_name}.txt", mode='a')
+    my_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    my_file = open(f"{file_name}.txt")
+    return my_file.read()
